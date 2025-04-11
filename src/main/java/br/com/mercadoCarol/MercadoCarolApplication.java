ca44abd59f5b4572d432e57c7c78a54109c08288
rolivsan <rolivsan@gmail.com>
2025-04-11 14:54:31 -0300

    first commit

---
+++ src/main/java/br/com/mercadoCarol/MercadoCarolApplication.java
@@ -0,0 +1,13 @@
+package br.com.mercadoCarol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoCarolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoCarolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mercadoCarol/MercadoCarolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadoCarol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoCarolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
